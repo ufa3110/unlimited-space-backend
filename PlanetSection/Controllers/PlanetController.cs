6bf7bbe696cf147c3f95f58c07ac9c63fb898ae4
URMANOV_AR <urmanov_ar@directum.ru>
2023-05-19 21:24:02 +0500

    add planet controller

---
+++ PlanetSection/Controllers/PlanetController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UnlimitedSpaceService.PlanetSection.Controllers
+{
+    [Route("api/planet")]
+    [ApiController]
+    public class PlanetController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public async Task<string> GetPlanetById(string id)
+        {
+            return $"planet-test-response planetId: {id}";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,24 @@
 var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
 
 builder.Services.AddMvc();
 builder.Services.AddSwaggerGen();
+
+
+//services
+//                .AddScoped<IS3Storage, S3Storage>()
+//                .AddScoped<IS3FileRepository, S3FileRepository>();
 
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 app.UseSwagger();
 app.UseSwaggerUI();
+
+app.UseRouting();
+app.MapControllers();
 
 app.Run();
