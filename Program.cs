d494f71fa7506ecc36268d71f35e8de8ea681739
URMANOV_AR <urmanov_ar@directum.ru>
2023-05-19 20:52:56 +0500

    use swagger

--- Program.cs
+++ Program.cs
@@ -1,6 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddMvc();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.Run();
